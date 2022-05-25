0350af3bd8483adecc89abf8b04ff9e530bf170f
mug896 <mug896@gmail.com>
2022-05-26 00:54:35 +0900

    update

--- dart-bash-completion.sh
+++ dart-bash-completion.sh
@@ -7,20 +7,13 @@ _dart()
     local SED_COM='sed -En '\''/^Available (sub)?commands:/,${ s/^  ([^[:blank:]]+).*/\1/p }'\'
     local SED_OPT='sed -En -e '\''s/^... (--[^[:blank:]<]+).*/\1/; tX; b'\'' -e '\'':X s/\[|\]//g; p; tY; b'\'' -e '\'':Y s/no-//p'\'
 
-    for (( i = 1; i < $COMP_CWORD; i++ )); do
-        if [[ ${COMP_WORDS[i]} = "=" || ${COMP_WORDS[i-1]} = "=" ]]; then
-            COM="$COM${COMP_WORDS[i]}"
-        else
-            COM="$COM ${COMP_WORDS[i]}"
-        fi
-    done
     if [ "${CUR:0:1}" = "-" ]; then
-        WORDS=$( eval "$COM --help" |& eval "$SED_OPT" )
+        WORDS=$( eval "${COMP_LINE% *} --help" |& eval "$SED_OPT" )
     else
         if [ "${COMP_CWORD}" -eq 1 ]; then
             WORDS=$( ${COMP_WORDS[0]} --help | eval "$SED_COM" )
         else
-            WORDS=$( eval "$COM --help" |& eval "$SED_COM" )
+            WORDS=$( eval "${COMP_LINE% *} --help" |& eval "$SED_COM" )
         fi
     fi
     [ -n "$WORDS" ] && COMPREPLY=( $(compgen -W "$WORDS" -- "$CUR") )
@@ -28,4 +21,5 @@ _dart()
 }
 
 complete -o default -F _dart dart
+
 
