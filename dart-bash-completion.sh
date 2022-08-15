f7e4bb6db95014a48f9a12db718bd19bb48949ca
Hyunho Cho <mug896@gmail.com>
2022-08-15 17:34:36 +0900

    update

--- dart-bash-completion.sh
+++ dart-bash-completion.sh
@@ -1,8 +1,7 @@
 
 _dart() 
 {
-    local COM=${COMP_WORDS[0]}
-    local CUR=${COMP_WORDS[COMP_CWORD]}
+    local CMD=$1 CUR=$2
     local IFS=$' \t\n' WORDS
     local SED_CMD='sed -En '\''/^Available (sub)?commands:/,${ s/^  ([^[:blank:]]+).*/\1/p }'\'
     local SED_OPT='sed -En -e '\''s/^ *((-\w), )?(-[^[:blank:]<]+).*/\2\n\3/; tX; b'\'' -e '\'':X s/\[|\]//g; p; tY; b'\'' -e '\'':Y s/no-//p'\'
