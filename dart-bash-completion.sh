425485a0cd5b0535aa752a04ac5b193e3f019965
Hyunho Cho <mug896@gmail.com>
2022-11-18 02:39:03 +0900

    update

--- dart-bash-completion.sh
+++ dart-bash-completion.sh
@@ -1,37 +1,37 @@
 
 _dart() 
 {
-    local CMD=$1 CUR=${COMP_WORDS[COMP_CWORD]}
-    [[ ${COMP_LINE:COMP_POINT-1:1} = " " ]] && CUR=""
-    local IFS=$' \t\n' WORDS
-    local SED_CMD SED_OPT
-    read -rd "" SED_CMD <<\@
+    local cmd=$1 cur=${COMP_WORDS[COMP_CWORD]}
+    [[ ${COMP_LINE:COMP_POINT-1:1} = " " ]] && cur=""
+    local IFS=$' \t\n' words
+    local sed_cmd sed_opt
+    read -rd "" sed_cmd <<\@
     sed -En '/^Available (sub)?commands:/,${ s/^  ([^[:blank:]]+).*/\1/p }'
 @
-    read -rd "" SED_OPT <<\@
+    read -rd "" sed_opt <<\@
     sed -En -e 's/^ *((-\w), )?(-[^[:blank:]<]+).*/\2\n\3/; tX; b' \
             -e ':X s/\[|\]//g; p; tY; b' -e ':Y s/no-//p'
 @
-    if [[ $CUR == -* ]]; then
-        if WORDS=$( eval "${COMP_LINE% *} --help" 2>&1 ); then
-            WORDS=$( <<< $WORDS eval "$SED_OPT" )
+    if [[ $cur == -* ]]; then
+        if words=$( eval "${COMP_LINE% *} --help" 2>&1 ); then
+            words=$( <<< $words eval "$sed_opt" )
         else
-            echo; <<< $WORDS head -n1 >&2
+            echo; <<< $words head -n1 >&2
             return
         fi
     else
         if (( COMP_CWORD == 1 )); then
-            WORDS=$( $CMD --help | eval "$SED_CMD" )
+            words=$( $cmd --help | eval "$sed_cmd" )
         else
-            if WORDS=$( eval "${COMP_LINE% *} --help" 2>&1 ); then
-                WORDS=$( <<< $WORDS eval "$SED_CMD" )
+            if words=$( eval "${COMP_LINE% *} --help" 2>&1 ); then
+                words=$( <<< $words eval "$sed_cmd" )
             else
-                echo; <<< $WORDS head -n1 >&2
+                echo; <<< $words head -n1 >&2
                 return
             fi
         fi
     fi
-    COMPREPLY=( $(compgen -W "$WORDS" -- "$CUR") )
+    COMPREPLY=( $(compgen -W "$words" -- "$cur") )
     [ "${COMPREPLY: -1}" = "=" ] && compopt -o nospace
 }
 
